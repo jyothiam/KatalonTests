f5c9a982226a605c4bf0df8fd8edab711ae10056
Jyothi <basava.jyothi@nxp.com>
2020-07-08 03:07:59 +0530

    second commit

---
+++ Test Suites/Test Suite1.ts
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test Suite1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>0c2f0464-cb6e-448d-b03d-19a4218e7d40</testSuiteGuid>
+   <testCaseLink>
+      <guid>e0e2bb87-ae91-4fa5-9dc8-ad73575c9d97</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test Case1</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>7f12fb12-2def-4014-b969-d727e94b189f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test case 2</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
